35e9a93204fe53e683417d2cfe77eb400cb15266
Polina <polinaucc@gmail.com>
2020-09-28 08:42:27 +0300

    initial commit

---
+++ src/main/java/ua/polina/smarthousemonitoringsystem/SmartHouseMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package ua.polina.smarthousemonitoringsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHouseMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHouseMonitoringSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/polina/smarthousemonitoringsystem/SmartHouseMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.polina.smarthousemonitoringsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHouseMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

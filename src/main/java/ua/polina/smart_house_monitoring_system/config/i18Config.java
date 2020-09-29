23c89adb3b58ab5db2a06a8fbcee858f6f5b295c
Polina <polinaucc@gmail.com>
2020-09-29 18:45:27 +0300

    added internationalization for registration and login pages

---
+++ src/main/java/ua/polina/smart_house_monitoring_system/config/i18Config.java
@@ -0,0 +1,48 @@
+package ua.polina.smart_house_monitoring_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+/**
+ * Configuration for internationalization.
+ */
+@Configuration
+public class i18Config implements WebMvcConfigurer {
+    /**
+     * Locale resolver
+     *
+     * @return the locale resolver
+     */
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+
+        cookieLocaleResolver.setDefaultLocale(Locale.US);
+        return cookieLocaleResolver;
+    }
+
+    /**
+     * Locale change interceptor
+     *
+     * @return the locale change interceptor
+     */
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}

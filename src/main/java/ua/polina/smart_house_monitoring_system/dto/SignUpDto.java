3216c4a213d102decc807d007ef2f8f43ef59cb4
Polina <polinaucc@gmail.com>
2020-09-30 12:26:37 +0300

    added localization for validation messages on the registration form

--- src/main/java/ua/polina/smart_house_monitoring_system/dto/SignUpDto.java
+++ src/main/java/ua/polina/smart_house_monitoring_system/dto/SignUpDto.java
@@ -15,44 +15,44 @@ import java.time.LocalDate;
 @Data
 public class SignUpDto {
     @NotBlank
-    @Size(min = 5, max = 20, message = "Username size error")
+    @Size(min = 5, max = 40, message = "{username.error}")
     private String username;
 
     @NotBlank
-    @Size(min = 5, max = 20, message = "Password size error")
+    @Size(min = 5, max = 20, message = "{password.error}")
     private String password;
 
     @NotBlank
-    @Pattern(regexp = "^[A-Z][a-z]{4,19}", message = "First name format error")
+    @Pattern(regexp = "^[A-Z][a-z]{4,19}", message = "{first.name.error}")
     private String firstName;
 
     @NotBlank
-    @Pattern(regexp = "^[А-ЩЮЯЭ][а-я]{3,19}$", message = "Russian first name format error")
+    @Pattern(regexp = "^[А-ЩЮЯЭ][а-я]{3,19}$", message = "{ru.error}")
     private String firstNameRu;
 
     @NotBlank
-    @Pattern(regexp = "^[A-Z][a-z]{4,19}", message = "Middle name format error")
+    @Pattern(regexp = "^[A-Z][a-z]{4,19}", message = "{middle.name.error}")
     private String middleName;
 
     @NotBlank
     @Pattern(regexp = "(^[А-ЩЮЯЭ][а-я]{5,19})(ович$|евич$|евна$|овна$)",
-            message = "Russian middle name format exception")
+            message = "{middle.name.ru.error}")
     private String middleNameRu;
 
     @NotBlank
-    @Pattern(regexp = "^[A-Z][a-z]{4,19}", message = "Last name format exception")
+    @Pattern(regexp = "^[A-Z][a-z]{4,19}", message = "{last.name.error}")
     private String lastName;
 
     @NotBlank
-    @Pattern(regexp = "^[А-ЩЮЯЭ][а-я]{4,20}$", message = "Russian last name format exception")
+    @Pattern(regexp = "^[А-ЩЮЯЭ][а-я]{4,20}$", message = "{ru.error}")
     private String lastNameRu;
 
     @NotBlank
-    @Pattern(regexp = "^[А-Я]{2}[0-9]{6}$", message = "Passport format exception")
+    @Pattern(regexp = "^[А-Я]{2}[0-9]{6}$", message = "{illegal.passport}")
     private String passport;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @Past(message = "Date should be in the past")
+    @Past(message = "{wrong.format}")
     private LocalDate birthday;
     private Boolean role;
 }
